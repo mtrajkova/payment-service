86de9a376ef7cdc14e8de0f1f904b66cdbc45886
marijatrajkova <marija.trajkova@endava.com>
2020-06-09 11:47:33 +0200

    Add Eureka discovery

--- src/main/java/com/payment/apipayment/ApiPaymentApplication.java
+++ src/main/java/com/payment/apipayment/ApiPaymentApplication.java
@@ -2,12 +2,14 @@ package com.payment.apipayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPaymentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiPaymentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPaymentApplication.class, args);
+    }
 
 }
